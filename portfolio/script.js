0e9073e62859e6be7c898890c87753a0f3e12451
PRAVEENjeevan <142286075+PRAVEENjeevan@users.noreply.github.com>
2024-04-16 23:04:01 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,14 @@
+document
+  .querySelector(".action__btns .Linkedin")
+  .addEventListener("click", function () {
+    window.open(
+      "https://www.linkedin.com/in/praveen-jeevanantham-12476b252",
+      "_blank"
+    );
+  });
+
+document
+  .querySelector(".action__btns .Github")
+  .addEventListener("click", function () {
+    window.open((href = "https://github.com/PRAVEENjeevan"), "_blank");
+  });
